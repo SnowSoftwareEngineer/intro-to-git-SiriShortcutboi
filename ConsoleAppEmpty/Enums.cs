31384e11291593a3f59a1b3ab871a90ab0b6966c
holden.anderson <holden.anderson@students.snow.edu>
2026-02-19 17:15:13 -0700

    define enums inside namespace GameEnums, put all that inside Enums.cs

---
+++ ConsoleAppEmpty/Enums.cs
@@ -0,0 +1,18 @@
+namespace GameEnums
+{
+                        //remember that calling enum in a foreach loop
+        public enum GameDifficulty// must be uppercase like this: "Enum"
+        {                            
+        Easy,
+        Medium, 
+        Hard,
+        }
+
+        public enum CharacterClass
+    {
+        Warrior,
+        Mage,
+        Rogue,
+    }
+
+} 
--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -5,3 +5,8 @@
         Console.WriteLine("Hello Mr. Jensen");
     }
 }
+
+/* public enum GameEnums //remember that calling enum in a foreach loop
+{                 // must be uppercase like this Enum
+    
+}    */
